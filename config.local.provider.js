232c9ffc2fa7293483aaecee3b96ace83b3745dd
Arman <riasiarman@yahoo.com>
2020-06-19 12:03:47 +0430

    Adding our description

--- config.local.provider.js
+++
@@ -1,9 +0,0 @@
-// const HDWalletProvider = require('truffle-hdwallet-provider-privkey')
-// const privKeys = [
-//     'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-// ]
-// const key = 'AiRbLoCfRoStOrNgE'
-
-module.exports = (network) => {
-    // return new HDWalletProvider(privKeys, `https://${network}.infura.io/${key}`)
-}
